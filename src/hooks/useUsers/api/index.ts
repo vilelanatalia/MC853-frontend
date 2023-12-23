aee0dfde5ce63715115d20bffa05bd2e617281d4
juliano <juliano.amadeu.lm@gmail.com>
2023-12-23 02:15:19 -0300

    remove old files

--- src/hooks/useUsers/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { api } from '../../../api';
-
-export async function getUsers(hugePayload: string[]) {
-    const response = await api.post('/users', hugePayload);
-
-    return response.data;
-}
